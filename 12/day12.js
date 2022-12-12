ce003247e46223ae71fa630e886b48df65fe0cf0
Gaetan <gaetan.gelineau@gmail.com>
2022-12-12 10:50:20 +0100

    day12

---
+++ 12/day12.js
@@ -0,0 +1,84 @@
+import * as fs from "fs/promises";
+
+const parseInput = async (test = false) => {
+  const fileContent = (
+    await fs.readFile(test ? "input_test.txt" : "input.txt")
+  ).toString();
+  const lines = fileContent.split("\n");
+  return lines.map((line) => line.split(""));
+};
+
+const getCharCode = (letter) => {
+  if (letter === "S") {
+    return "a".charCodeAt();
+  }
+  if (letter === "E") {
+    return "z".charCodeAt();
+  }
+  return letter.charCodeAt();
+};
+
+const getNeighbors = (grid, pos, visitedPos) => {
+  const HEIGHT = grid.length;
+  const WIDTH = grid[0].length;
+  const { x, y, distance } = pos;
+
+  const neighbors = [];
+  if (
+    x > 0 &&
+    !visitedPos[`${x - 1}|${y}`] &&
+    getCharCode(grid[y][x - 1]) - getCharCode(grid[y][x]) <= 1
+  ) {
+    neighbors.push({ x: x - 1, y: y, distance: distance + 1 });
+  }
+
+  if (
+    x < WIDTH - 1 &&
+    !visitedPos[`${x + 1}|${y}`] &&
+    getCharCode(grid[y][x + 1]) - getCharCode(grid[y][x]) <= 1
+  ) {
+    neighbors.push({ x: x + 1, y: y, distance: distance + 1 });
+  }
+
+  if (
+    y > 0 &&
+    !visitedPos[`${x}|${y - 1}`] &&
+    getCharCode(grid[y - 1][x]) - getCharCode(grid[y][x]) <= 1
+  ) {
+    neighbors.push({ x: x, y: y - 1, distance: distance + 1 });
+  }
+
+  if (
+    y < HEIGHT - 1 &&
+    !visitedPos[`${x}|${y + 1}`] &&
+    getCharCode(grid[y + 1][x]) - getCharCode(grid[y][x]) <= 1
+  ) {
+    neighbors.push({ x: x, y: y + 1, distance: distance + 1 });
+  }
+
+  return neighbors;
+};
+
+const grid = await parseInput(false);
+
+// const START_POS = { x: 0, y: 0, distance: 0 };
+const START_POS = { x: 0, y: 20, distance: 0 };
+const visitedPos = {};
+
+const queue = [START_POS];
+while (queue.length > 0) {
+  const { x, y, distance } = queue.shift();
+  console.log(distance);
+  if (visitedPos[`${x}|${y}`]) {
+    continue;
+  }
+
+  visitedPos[`${x}|${y}`] = true;
+  queue.push(...getNeighbors(grid, { x, y, distance }, visitedPos));
+  if (grid[y][x] === "E") {
+    console.log("Found it : ", x, y, distance);
+    break;
+  }
+}
+
+console.log("Graph search ended");
