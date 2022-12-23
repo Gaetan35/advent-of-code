b1826dbe9522fd9dce5b2096422d71ce53e5c6f1
Gaetan <gaetan.gelineau@gmail.com>
2022-12-23 09:12:23 +0100

    day22: part 2

--- 22/day22_2.js
+++ 22/day22_2.js
@@ -26,34 +26,6 @@ const parseInput = async (test = false) => {
     }
   }
 
-  const rows = {};
-  for (let i = 0; i < gridLines.length; i++) {
-    const existingIndexes = [];
-    for (let j = 0; j < gridWidth; j++) {
-      if (grid[i][j] !== " ") {
-        existingIndexes.push(j);
-      }
-    }
-    rows[i] = {
-      left: Math.min(...existingIndexes),
-      right: Math.max(...existingIndexes),
-    };
-  }
-
-  const columns = {};
-  for (let j = 0; j < gridWidth; j++) {
-    const existingIndexes = [];
-    for (let i = 0; i < gridLines.length; i++) {
-      if (grid[i][j] !== " ") {
-        existingIndexes.push(i);
-      }
-    }
-    columns[j] = {
-      up: Math.min(...existingIndexes),
-      down: Math.max(...existingIndexes),
-    };
-  }
-
   const startIndex = grid[0].indexOf(".");
 
   const pathInstructionsRaw = lines[lines.length - 1];
@@ -71,7 +43,7 @@ const parseInput = async (test = false) => {
       isNaN(instruction) ? instruction : parseInt(instruction)
     );
 
-  return { pathInstructions, grid, startIndex, columns, rows };
+  return { pathInstructions, grid, startIndex };
 };
 
 const getNewFacing = (previousFacing, rotateDirection) => {
@@ -88,53 +60,129 @@ const getNewFacing = (previousFacing, rotateDirection) => {
   }
 };
 
-const getWrappedPos = (x, y, facing, columns, rows) => {
-  const faceSize = 4;
-
+const getWrappedPos = (x, y, facing) => {
+  const faceSize = 50;
+  // console.log("inputs : ", x, y, facing);
   const faceX = Math.floor(x / faceSize);
   const faceY = Math.floor(y / faceSize);
   const faceIndex = {
-    "2-0": 1,
-    "0-1": 2,
+    "1-0": 1,
+    "2-0": 2,
     "1-1": 3,
-    "2-1": 4,
-    "2-2": 5,
-    "3-2": 6,
+    "0-2": 4,
+    "1-2": 5,
+    "0-3": 6,
   }[`${faceX}-${faceY}`];
   const newPosition = {
-    4: {
-      right: (oldX, oldY) => ({
-        wrappedX: 4 * faceSize - 1 - (oldY % faceSize),
-        wrappedY: 2 * faceSize,
-        wrappedFacing: "down",
+    1: {
+      left: (oldX, oldY) => ({
+        // 4 facing right
+        wrappedX: 0,
+        wrappedY: 3 * faceSize - 1 - (oldY % faceSize),
+        wrappedFacing: "right",
+      }),
+      up: (oldX, oldY) => ({
+        // 6 facing right
+        wrappedX: 0,
+        wrappedY: 3 * faceSize + (oldX % faceSize),
+        wrappedFacing: "right",
       }),
     },
-    5: {
+    2: {
       down: (oldX, oldY) => ({
-        wrappedX: faceSize - 1 - (oldX % faceSize),
-        wrappedY: 2 * faceSize - 1,
+        // 3 facing left
+        wrappedX: 2 * faceSize - 1,
+        wrappedY: faceSize + (oldX % faceSize),
+        wrappedFacing: "left",
+      }),
+      right: (oldX, oldY) => ({
+        // 5 facing left
+        wrappedX: 2 * faceSize - 1,
+        wrappedY: 3 * faceSize - 1 - (oldY % faceSize),
+        wrappedFacing: "left",
+      }),
+      up: (oldX, oldY) => ({
+        // 6 facing up
+        wrappedX: oldX % faceSize,
+        wrappedY: 4 * faceSize - 1,
         wrappedFacing: "up",
       }),
     },
     3: {
+      left: (oldX, oldY) => ({
+        // 4 facing down
+        wrappedX: oldY % faceSize,
+        wrappedY: 2 * faceSize,
+        wrappedFacing: "down",
+      }),
+      right: (oldX, oldY) => ({
+        // 2 facing up
+        wrappedX: 2 * faceSize + (oldY % faceSize),
+        wrappedY: faceSize - 1,
+        wrappedFacing: "up",
+      }),
+    },
+    4: {
+      left: (oldX, oldY) => ({
+        // 1 facing right
+        wrappedX: faceSize,
+        wrappedY: faceSize - 1 - (oldY % faceSize),
+        wrappedFacing: "right",
+      }),
       up: (oldX, oldY) => ({
-        wrappedX: 2 * faceSize,
-        wrappedY: oldX % faceSize,
+        // 3 facing right
+        wrappedX: faceSize,
+        wrappedY: faceSize + (oldX % faceSize),
         wrappedFacing: "right",
       }),
     },
+    5: {
+      right: (oldX, oldY) => ({
+        // 2 facing left
+        wrappedX: 3 * faceSize - 1,
+        wrappedY: faceSize - 1 - (oldY % faceSize),
+        wrappedFacing: "left",
+      }),
+      down: (oldX, oldY) => ({
+        // 6 facing left
+        wrappedX: faceSize - 1,
+        wrappedY: 3 * faceSize + (oldX % faceSize),
+        wrappedFacing: "left",
+      }),
+    },
+    6: {
+      left: (oldX, oldY) => ({
+        // 1 facing down
+        wrappedX: faceSize + (oldY % faceSize),
+        wrappedY: 0,
+        wrappedFacing: "down",
+      }),
+      right: (oldX, oldY) => ({
+        // 5 facing up
+        wrappedX: faceSize + (oldY % faceSize),
+        wrappedY: 3 * faceSize - 1,
+        wrappedFacing: "up",
+      }),
+      down: (oldX, oldY) => ({
+        // 2 facing down
+        wrappedX: 2 * faceSize + (oldX % faceSize),
+        wrappedY: 0,
+        wrappedFacing: "down",
+      }),
+    },
   };
-
+  // console.log(faceIndex, facing);
   return newPosition[faceIndex][facing](x, y);
 };
 
-const move = ({ x, y, facing }, grid, distance, columns, rows) => {
+const move = ({ x, y, facing }, grid, distance) => {
   const movements = {
     right: { dx: 1, dy: 0 },
     left: { dx: -1, dy: 0 },
     up: { dx: 0, dy: -1 },
     down: { dx: 0, dy: 1 },
   };
+  const facingArrow = { right: ">", left: "<", up: "^", down: "v" };
   let currentX = x;
   let currentY = y;
   let currentFacing = facing;
@@ -147,9 +195,7 @@ const move = ({ x, y, facing }, grid, distance, columns, rows) => {
       const { wrappedFacing, wrappedX, wrappedY } = getWrappedPos(
         currentX,
         currentY,
-        facing,
-        columns,
-        rows
+        facing
       );
       newX = wrappedX;
       newY = wrappedY;
@@ -165,21 +211,62 @@ const move = ({ x, y, facing }, grid, distance, columns, rows) => {
   return { newX: currentX, newY: currentY, newFacing: currentFacing };
 };
 
-const { pathInstructions, grid, startIndex, columns, rows } = await parseInput(
-  true
-);
+const { pathInstructions, grid, startIndex } = await parseInput(false);
+
+// const faceIndexes = Array.from({ length: grid.length }, (_, i) =>
+//   Array.from({ length: grid[0].length }, (_, j) => {
+//     const faceX = Math.floor(j / 50);
+//     const faceY = Math.floor(i / 50);
+//     const faceIdByIndex = {
+//       "1-0": 1,
+//       "2-0": 2,
+//       "1-1": 3,
+//       "0-2": 4,
+//       "1-2": 5,
+//       "0-3": 6,
+//     };
+//     const faceIndex = faceIdByIndex[`${faceX}-${faceY}`];
+//     if (grid[i][j] === " ") return " ";
+//     if (grid?.[i + 1]?.[j] === " " || grid?.[i + 1]?.[j] === undefined) {
+//       const { wrappedX, wrappedY } = getWrappedPos(j, i, "down");
+//       console.log(wrappedX, wrappedY);
+//       return faceIdByIndex[
+//         `${Math.floor(wrappedX / 50)}-${Math.floor(wrappedY / 50)}`
+//       ];
+//     }
+//     if (grid?.[i - 1]?.[j] === " " || grid?.[i - 1]?.[j] === undefined) {
+//       const { wrappedX, wrappedY } = getWrappedPos(j, i, "up");
+//       console.log(wrappedX, wrappedY);
+
+//       return faceIdByIndex[
+//         `${Math.floor(wrappedX / 50)}-${Math.floor(wrappedY / 50)}`
+//       ];
+//     }
+//     if (grid?.[i]?.[j + 1] === " " || grid?.[i]?.[j + 1] === undefined) {
+//       const { wrappedX, wrappedY } = getWrappedPos(j, i, "right");
+//       console.log(wrappedX, wrappedY);
+
+//       return faceIdByIndex[
+//         `${Math.floor(wrappedX / 50)}-${Math.floor(wrappedY / 50)}`
+//       ];
+//     }
+//     if (grid?.[i]?.[j - 1] === " " || grid?.[i]?.[j - 1] === undefined) {
+//       const { wrappedX, wrappedY } = getWrappedPos(j, i, "left");
+//       console.log(wrappedX, wrappedY);
+
+//       return faceIdByIndex[
+//         `${Math.floor(wrappedX / 50)}-${Math.floor(wrappedY / 50)}`
+//       ];
+//     }
+//     return faceIndex;
+//   })
+// );
+// displayGrid(faceIndexes);
 
 const currentPosition = { y: 0, x: startIndex, facing: "right" };
-
 for (const instruction of pathInstructions) {
   if (typeof instruction === "number") {
-    const { newX, newY, newFacing } = move(
-      currentPosition,
-      grid,
-      instruction,
-      columns,
-      rows
-    );
+    const { newX, newY, newFacing } = move(currentPosition, grid, instruction);
     currentPosition.x = newX;
     currentPosition.y = newY;
     currentPosition.facing = newFacing;
@@ -187,9 +274,6 @@ for (const instruction of pathInstructions) {
     currentPosition.facing = getNewFacing(currentPosition.facing, instruction);
   }
 }
-
-grid[currentPosition.y][currentPosition.x] = "A";
-displayGrid(grid);
 
 const facingToNumber = {
   right: 0,
@@ -202,3 +286,6 @@ const result = (y + 1) * 1000 + (x + 1) * 4 + facingToNumber[facing];
 
 console.log(currentPosition);
 console.log("Result : ", result);
+
+// first try : 8279 is too low
+// Second try: 148052 is too high
