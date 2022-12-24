cdb737ae878cffa328a24d6908c2f34d20d5e1ae
Gaetan <gaetan.gelineau@gmail.com>
2022-12-24 14:42:26 +0100

    day24: part 1

--- 24/day24.js
+++ 24/day24.js
@@ -1,5 +1,4 @@
 import * as fs from "fs/promises";
-import Queue from "queue";
 
 const parseInput = async (test = false) => {
   const fileContent = (
@@ -85,28 +84,19 @@ const possibleMoves = [
   { dx: 0, dy: 1 },
 ];
 
+const CACHE = {};
+
 const computeDistance = (grid) => {
   const width = grid[0].length;
   const height = grid.length;
   const startPos = { x: grid[0].indexOf("."), y: 0 };
   const endPos = { x: grid[height - 1].indexOf("."), y: height - 1 };
   const nodes = [{ x: startPos.x, y: startPos.y, minute: 0 }];
-  let minDistance = 1823;
+
+  let minDistance;
   let iterations = 0;
   while (nodes.length > 0) {
     iterations += 1;
-    // nodes.sort(
-    //   (
-    //     { x: x1, y: y1, minute: minute1 },
-    //     { x: x2, y: y2, minute: minute2 }
-    //   ) => {
-    //     const score1 =
-    //       Math.abs(endPos.x - x1) + Math.abs(endPos.y - y1) + minute1;
-    //     const score2 =
-    //       Math.abs(endPos.x - x2) + Math.abs(endPos.y - y2) + minute2;
-    //     return score2 - score1;
-    //   }
-    // );
     const { x, y, minute: nodeMinute } = nodes.shift();
 
     if (iterations % 10000 === 0) {
@@ -131,6 +121,11 @@ const computeDistance = (grid) => {
       continue;
     }
 
+    if (CACHE[`${x}|${y}|${nodeMinute}`]) {
+      continue;
+    }
+    CACHE[`${x}|${y}|${nodeMinute}`] = true;
+
     const vortexes = computeVortexesForMinute(width, height, nodeMinute + 1);
 
     for (const { dx, dy } of possibleMoves) {
@@ -151,20 +146,8 @@ const computeDistance = (grid) => {
   return minDistance;
 };
 
-const queue = Queue({ results: [] });
-queue.push(3);
-queue.push(5);
-queue.push(7);
-console.log(queue.shift());
-queue.shift();
-queue.shift();
-queue.shift();
-console.log(queue.length);
-
-// const grid = await parseInput(false);
-// const startingVortexes = getVortexes(grid);
-// const VORTEXES_BY_MINUTE = [startingVortexes];
-// const result = computeDistance(grid, startingVortexes);
-// console.log("Result : ", result);
-
-// First try: 1823 is too high
+const grid = await parseInput(false);
+const startingVortexes = getVortexes(grid);
+const VORTEXES_BY_MINUTE = [startingVortexes];
+const result = computeDistance(grid, startingVortexes);
+console.log("Result : ", result);
