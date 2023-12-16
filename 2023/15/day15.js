1217aec8b3de622ac5db98160b784fbdf5b45f02
Gaetan <gaetan.gelineau@gmail.com>
2023-12-16 08:39:50 +0100

    day15: part2

--- 2023/15/day15.js
+++ 2023/15/day15.js
@@ -20,10 +20,50 @@ const computeHashValue = (sequence) => {
 
 const input = await parseTextInput(false);
 
-let hashSum = 0;
+// let part1Result = 0;
+// for (const step of input) {
+//   const hash = computeHashValue(step);
+//   part1Result += hash;
+// }
+// console.log(part1Result);
+
+const computeLensLabel = (step) => step.split("-")[0].split("=")[0];
+
+const boxes = {};
 for (const step of input) {
-  const hash = computeHashValue(step);
-  hashSum += hash;
-  // console.log(`${step}: hash = ${hash}`);
+  const lensLabel = computeLensLabel(step);
+  const boxNumber = computeHashValue(lensLabel);
+  const operation = step[lensLabel.length];
+
+  if (operation === "=") {
+    if (!boxes[boxNumber]) {
+      boxes[boxNumber] = [step];
+    } else {
+      const sameLabelBoxIndex = boxes[boxNumber].findIndex(
+        (stepInBox) => computeLensLabel(stepInBox) === lensLabel
+      );
+      if (sameLabelBoxIndex === -1) {
+        boxes[boxNumber].push(step);
+      } else {
+        boxes[boxNumber][sameLabelBoxIndex] = step;
+      }
+    }
+  } else if (operation === "-") {
+    if (boxes[boxNumber]) {
+      boxes[boxNumber] = boxes[boxNumber].filter(
+        (stepInBox) => computeLensLabel(stepInBox) !== lensLabel
+      );
+    }
+  }
 }
-console.log(hashSum);
+
+let focusingPower = 0;
+for (const [boxNumber, steps] of Object.entries(boxes)) {
+  for (let i = 0; i < steps.length; i++) {
+    const step = steps[i];
+    const toAdd =
+      (Number(boxNumber) + 1) * (i + 1) * Number(step[step.length - 1]);
+    focusingPower += toAdd;
+  }
+}
+console.log(focusingPower);
