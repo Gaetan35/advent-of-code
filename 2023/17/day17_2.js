29707179b8edac16c083dedc2ffba04aaefd6150
Gaetan <gaetan.gelineau@gmail.com>
2023-12-21 09:06:41 +0100

    day17: wip

--- 2023/17/day17_2.js
+++ 2023/17/day17_2.js
@@ -1,5 +1,5 @@
-import { writeFileSync } from "fs";
 import * as fs from "fs/promises";
+import { writeFileSync } from "fs";
 
 const prettyPrint = (grid) => {
   console.log(grid.map((line) => line.join("|")).join("\n"));
@@ -15,43 +15,68 @@ const parseTextInput = async (isTest = false) => {
 };
 
 const deltasForLastDirection = {
-  RIGHT: [
-    ["RIGHT", 1, 0],
-    ["BOTTOM", 0, 1],
-    ["TOP", 0, -1],
-  ],
-  LEFT: [
-    ["BOTTOM", 0, 1],
-    ["LEFT", -1, 0],
-    ["TOP", 0, -1],
-  ],
-  TOP: [
-    ["RIGHT", 1, 0],
-    ["LEFT", -1, 0],
-    ["TOP", 0, -1],
-  ],
-  BOTTOM: [
-    ["RIGHT", 1, 0],
-    ["BOTTOM", 0, 1],
-    ["LEFT", -1, 0],
-  ],
-  START: [
-    ["RIGHT", 1, 0],
-    ["BOTTOM", 0, 1],
-  ],
+  RIGHT: {
+    true: [
+      ["TOP", 0, -1],
+      ["BOTTOM", 0, 1],
+    ],
+    false: [
+      ["TOP", 0, -1],
+      ["BOTTOM", 0, 1],
+      ["RIGHT", 1, 0],
+    ],
+  },
+  LEFT: {
+    true: [
+      ["TOP", 0, -1],
+      ["BOTTOM", 0, 1],
+    ],
+    false: [
+      ["LEFT", -1, 0],
+      ["TOP", 0, -1],
+      ["BOTTOM", 0, 1],
+    ],
+  },
+  TOP: {
+    true: [
+      ["LEFT", -1, 0],
+      ["RIGHT", 1, 0],
+    ],
+    false: [
+      ["LEFT", -1, 0],
+      ["TOP", 0, -1],
+      ["RIGHT", 1, 0],
+    ],
+  },
+  BOTTOM: {
+    true: [
+      ["LEFT", -1, 0],
+      ["RIGHT", 1, 0],
+    ],
+    false: [
+      ["LEFT", -1, 0],
+      ["BOTTOM", 0, 1],
+      ["RIGHT", 1, 0],
+    ],
+  },
+  START: {
+    false: [
+      ["BOTTOM", 0, 1],
+      ["RIGHT", 1, 0],
+    ],
+  },
 };
 const computeNeighbors = (grid, [x, y], lastDirections, heatLoss) => {
-  let deltas = deltasForLastDirection[lastDirections.at(-1)];
-  if (
+  const areSameThreeLastDirections =
     lastDirections.length === 3 &&
-    lastDirections[0] === lastDirections[1] &&
-    lastDirections[1] === lastDirections[2]
-  ) {
-    // TODO: change object above to remove that filter
-    deltas = deltas.filter(([direction]) => direction !== lastDirections[2]);
-  }
+    (lastDirections[0] === lastDirections[1] ||
+      lastDirections[0] === "START") &&
+    lastDirections[1] === lastDirections[2];
+
   const neighbors = [];
-  for (const [direction, dx, dy] of deltas) {
+  for (const [direction, dx, dy] of deltasForLastDirection[
+    lastDirections.at(-1)
+  ][areSameThreeLastDirections]) {
     const newX = x + dx;
     const newY = y + dy;
     if (grid[newY]?.[newX] !== undefined) {
@@ -68,19 +93,24 @@ const computeNeighbors = (grid, [x, y], lastDirections, heatLoss) => {
 const getCacheKey = (x, y, lastDirections) => {
   const areSameThreeLastDirections =
     lastDirections.length === 3 &&
-    lastDirections[0] === lastDirections[1] &&
+    (lastDirections[0] === lastDirections[1] ||
+      lastDirections[0] === "START") &&
     lastDirections[1] === lastDirections[2];
-  return `${x}|${y}|${lastDirections.at(-1)}|${areSameThreeLastDirections}`;
+
+  const areSameTwoLastDirections =
+    lastDirections.length >= 2 &&
+    (lastDirections[0] === lastDirections[1] || lastDirections[0] === "START");
+
+  return `${x}|${y}|${lastDirections.at(
+    -1
+  )}|3${areSameThreeLastDirections}|2${areSameTwoLastDirections}`;
 };
 
 const findMinHeatLoss = (grid, minHeatLossRemainingGrid) => {
   const HEIGHT = grid.length;
   const WIDTH = grid[0].length;
-
   const cache = {};
-
-  // 965 is best path found yet
-  let minHeatLoss = 1000;
+  let minHeatLoss = 965;
   console.log("Starting with minHeatLoss: ", minHeatLoss);
   const nodes = [
     {
@@ -89,10 +119,11 @@ const findMinHeatLoss = (grid, minHeatLossRemainingGrid) => {
       heatLoss: 0,
     },
   ];
+
   let i = 0;
   while (nodes.length > 0) {
     i += 1;
-    if (i % 10000000 === 0) {
+    if (i % 1000000 === 0) {
       console.log(`${i}: ${nodes.length} nodes`);
     }
     const {
@@ -101,28 +132,41 @@ const findMinHeatLoss = (grid, minHeatLossRemainingGrid) => {
       heatLoss,
     } = nodes.pop();
 
+    if (x === WIDTH - 1 && y === HEIGHT - 1) {
+      if (heatLoss < minHeatLoss) {
+        // console.log("\nFound new min value: ", heatLoss, "\n");
+        minHeatLoss = heatLoss;
+      }
+      continue;
+    }
+
     const cacheKey = getCacheKey(x, y, lastDirections);
+
+    // const cacheKeyFalse = getCacheKey(x, y, lastDirections.at(-1), false);
 
     if (cache[cacheKey] !== undefined && cache[cacheKey] < heatLoss) {
       continue;
     }
     cache[cacheKey] = heatLoss;
 
-    // TODO: exit condition should be before cache
-    if (x === WIDTH - 1 && y === HEIGHT - 1) {
-      if (minHeatLoss === null || heatLoss < minHeatLoss) {
-        console.log("\nFound new min value: ", heatLoss, "\n");
-        minHeatLoss = heatLoss;
-      }
-      continue;
-    }
-
     if (heatLoss + minHeatLossRemainingGrid[y][x] > minHeatLoss) {
       continue;
     }
 
-    nodes.push(...computeNeighbors(grid, [x, y], lastDirections, heatLoss));
+    nodes.push(
+      ...computeNeighbors(grid, [x, y], lastDirections, heatLoss).filter(
+        (neighbor) => {
+          const cacheKey = getCacheKey(
+            neighbor.pos[0],
+            neighbor.pos[1],
+            neighbor.lastDirections
+          );
+          return cache[cacheKey] === undefined || heatLoss < cache[cacheKey];
+        }
+      )
+    );
   }
+  console.log(i);
 
   return minHeatLoss;
 };
@@ -164,7 +208,11 @@ const computeMinHeatLossRemainingGrid = (grid) => {
     for (const [dx, dy] of deltas) {
       const newX = x + dx;
       const newY = y + dy;
-      if (grid[newY]?.[newX] !== undefined) {
+      const newHeatLoss = heatLoss + grid[y][x];
+      if (
+        grid[newY]?.[newX] !== undefined &&
+        newHeatLoss < minHeatLossRemainingGrid[newY][newX]
+      ) {
         nodes.push({
           pos: [newX, newY],
           heatLoss: heatLoss + grid[y][x],
@@ -172,16 +220,18 @@ const computeMinHeatLossRemainingGrid = (grid) => {
       }
     }
   }
+  console.log(nodes);
   return minHeatLossRemainingGrid;
 };
 
 const input = await parseTextInput(false);
+
 // console.log("Computing grid ...");
 // const minHeatLossRemainingGrid = computeMinHeatLossRemainingGrid(input);
 
+// prettyPrint(minHeatLossRemainingGrid);
 // writeFileSync("grid.json", JSON.stringify(minHeatLossRemainingGrid));
 
-// prettyPrint(minHeatLossRemainingGrid);
 const minHeatLossRemainingGrid = JSON.parse(
   (await fs.readFile("gridReal.json")).toString()
 );
@@ -189,7 +239,5 @@ const minHeatLossRemainingGrid = JSON.parse(
 console.time();
 const minHeatLoss = findMinHeatLoss(input, minHeatLossRemainingGrid);
 console.timeEnd();
-console.log("Min heat loss: ", minHeatLoss);
 
-// 968 is too high
-// 965 is too high
+console.log("Min heat loss: ", minHeatLoss);
