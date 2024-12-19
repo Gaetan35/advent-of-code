5c0c2f1f1b0017b07f55d1f6ab8dcdcb265e47cf
Gaetan <gaetan.gelineau@gmail.com>
2024-12-19 09:53:36 +0100

    day19: part1

---
+++ 2024/19/day19.ts
@@ -0,0 +1,59 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+type Input = { patterns: string[]; designs: string[] };
+
+const parseTextInput = async (isTest = false): Promise<Input> => {
+  const filePath = path.join(
+    __dirname,
+    isTest ? "input_test.txt" : "input.txt"
+  );
+
+  const [patternsPart, designPart] = (await fs.readFile(filePath))
+    .toString()
+    .split("\n\n");
+
+  return {
+    patterns: patternsPart.split(", "),
+    designs: designPart.split("\n"),
+  };
+};
+
+function part1({ patterns, designs }: Input) {
+  let possibleDesigns = 0;
+
+  for (const design of designs) {
+    const nodes = [""];
+    while (nodes.length) {
+      const node = nodes.pop();
+      if (node === design) {
+        possibleDesigns++;
+        break;
+      }
+
+      for (const pattern of patterns) {
+        if (design.endsWith(pattern + node)) {
+          nodes.push(pattern + node);
+        }
+      }
+    }
+  }
+  return possibleDesigns;
+}
+
+function part2(input: Input) {
+  return null;
+}
+
+async function main() {
+  const IS_TEST = process.argv[2] === "test";
+  const input = await parseTextInput(IS_TEST);
+
+  const part1Result = part1(input);
+  console.log("Part1 result: ", part1Result);
+
+  const part2Result = part2(input);
+  console.log("Part2 result: ", part2Result);
+}
+
+main();
